7e27a6e125efbf0b5c617d578964fe5ea8d5dd17
Murat Kubekov <49613232+Clorhexidinum@users.noreply.github.com>
2022-09-02 12:21:28 +0300

    commit for conflict

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -13,3 +13,6 @@ def test_search_selene_in_google():
 def test_wrong_search_selene_in_google():
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
     browser.element('[id="search"]').should(have.no.text('Ответы других учеников'))
+
+def test_conflict_merge():
+    pass
