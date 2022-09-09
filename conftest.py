c883bed630de769d65be82ae564fc8787ec6ead8
Murat Kubekov <49613232+Clorhexidinum@users.noreply.github.com>
2022-09-09 15:54:24 +0300

    add student reg test

--- conftest.py
+++ conftest.py
@@ -1,26 +1,16 @@
+import os
+
 import pytest
-from selene.support.shared import browser, config
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-@pytest.fixture
-def get_chrome_options():
-    options = Options()
-    # options.add_argument('chrome')  # Use headless if you do not need a browser UI
-    # options.add_argument('--start-maximized')
-    # options.add_argument('--window-size=1920,1080')
-    # options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    options.page_load_strategy = 'eager'
-    return options
+from selene.support.shared import browser
 
 
-@pytest.fixture()
-def get_webdriver(get_chrome_options):
-    browser.config.driver = webdriver.Chrome(options=get_chrome_options)
-    return browser.config.driver
-
-@pytest.fixture()
-def open_browser(request, get_webdriver):
-    browser.open('https://staging-ru.maximumtest.test01.maximumtest.ru/ege')
-    yield
-    browser.close_current_tab()
+@pytest.fixture(scope='function', autouse=True)
+def browser_management():
+    browser.config.base_url = os.getenv('selene.base_url', 'https://demoqa.com')
+    browser.config.browser_name = os.getenv('selene.browser_name', 'chrome')
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    browser.config.hold_browser_open = (
+            os.getenv('selene.hold_browser_open', 'false').lower() == 'true'
+    )
+    browser.config.timeout = float(os.getenv('selene.timeout', '5'))
---
+++ tests/test_student_registration_form.py
@@ -0,0 +1,52 @@
+import os
+from selene.support.shared import browser
+from selene import have, be
+
+
+def test_submit_form():
+    browser.open('/automation-practice-form')
+
+    browser.element('#firstName').type('Murat')
+    browser.element('#lastName').type('Kubekov')
+    browser.element('#userEmail').type('test@gmail.com')
+
+    browser.element('#gender-radio-1 + label').click()
+
+    browser.element('#userNumber').type('89777777777')
+
+    # Там где 2 варианта прошу дать комментарий как более правильно
+    # browser.element('#dateOfBirthInput').should(be.blank).set_value("30 Oct 1991").press_enter()
+    browser.element('.react-datepicker-wrapper').click()
+    browser.element('.react-datepicker__month-select').type("October").press_enter()
+    browser.element('.react-datepicker__year-select').type("1991").press_enter()
+    browser.element('.react-datepicker__day[aria-label *= "October 30"]').click()
+    # browser.all('.react-datepicker__day').filter_by(have.text('30'))[1].click()
+
+    browser.element('#subjectsInput').type('Computer Science').press_enter().type('English').press_enter()
+
+    browser.element('#hobbies-checkbox-1 + label').click()
+    browser.element('#hobbies-checkbox-2 + label').click()
+    browser.element('#hobbies-checkbox-3 + label').click()
+
+    browser.element('#uploadPicture').send_keys(os.path.abspath('test_files/for_test_dont_remove.jpg'))
+
+    browser.element('#currentAddress').type('Moskow, Ulitsa Pushkina, dom Kolotushkina')
+    browser.element('#state').click()
+    browser.element('#state input').type('NCR').press_enter()
+    browser.element('#city').click()
+    browser.element('#city input').type('Delhi').press_enter()
+
+    browser.element('#submit').click()
+
+    browser.element('.modal-content').should(be.visible)
+    browser.elements('.modal-body td+td').should(have.texts(
+        'Murat Kubekov',
+        'test@gmail.com',
+        'Male',
+        '8977777777',
+        '30 October,1991',
+        'Computer Science, English',
+        'Sports, Reading, Music',
+        'for_test_dont_remove.jpg',
+        'Moskow, Ulitsa Pushkina, dom Kolotushkina',
+        'NCR Delhi'))
